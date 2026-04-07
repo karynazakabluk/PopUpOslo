8be2ac5a74bd65dac7b583cc8d034c32fa40ab67
karynazakabluk <karynazakabluk@gmail.com>
2026-04-07 23:32:43 +0200

    feat: added core domain entities

--- Domain/Entities/Booking.cs
+++ Domain/Entities/Booking.cs
@@ -1,6 +1,14 @@
-namespace DefaultNamespace;
+using PopUpOslo.Domain.Enums;
+
+namespace PopUpOslo.Domain.Entities;
 
 public class Booking
 {
-    
+    public int BookingId { get; set; }
+    public int UserId { get; set; }
+    public int EventId { get; set; }
+    public int OptionId { get; set; }
+    public decimal PriceAtBooking { get; set; }
+    public DateTime BookedAt { get; set; }
+    public BookingStatus Status { get; set; } = BookingStatus.Confirmed;
 }
--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -1,6 +1,8 @@
-namespace DefaultNamespace;
+namespace PopUpOslo.Domain.Entities;
 
 public class User
 {
-    
+    public int UserId { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
 }
