2565dc6efe23321853eb76edcf4436c3ec19a19a
MariaVilma12 <vilmashekar26@gmail.com>
2026-04-08 12:03:47 +0200

    Implemented data layer with repositories

---
+++ Infrastructure/Repositories/Database.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.SQLite;
+
+public static class Database
+{
+    private static string connectionString = "Data Source=Database/database.db";
+
+    public static SQLiteConnection GetConnection()
+    {
+        return new SQLiteConnection(connectionString);
+    }
+}
--- Infrastructure/Repositories/UserRepository.cs
+++ Infrastructure/Repositories/UserRepository.cs
@@ -1,6 +1,68 @@
-namespace DefaultNamespace;
+using System.Data.SQLite;
 
 public class UserRepository
 {
-    
+    public void AddUser(User user)
+    {
+        using var conn = Database.GetConnection();
+        conn.Open();
+
+        var cmd = conn.CreateCommand();
+        cmd.CommandText = "INSERT INTO Users (Username, Password, Role) VALUES (@u, @p, @r)";
+        cmd.Parameters.AddWithValue("@u", user.Username);
+        cmd.Parameters.AddWithValue("@p", user.Password);
+        cmd.Parameters.AddWithValue("@r", user.Role);
+
+        cmd.ExecuteNonQuery();
+    }
+
+    public User GetUserByUsername(string username)
+    {
+        using var conn = Database.GetConnection();
+        conn.Open();
+
+        var cmd = conn.CreateCommand();
+        cmd.CommandText = "SELECT * FROM Users WHERE Username = @u";
+        cmd.Parameters.AddWithValue("@u", username);
+
+        using var reader = cmd.ExecuteReader();
+
+        if (reader.Read())
+        {
+            return new User
+            {
+                UserId = reader.GetInt32(0),
+                Username = reader.GetString(1),
+                Password = reader.GetString(2),
+                Role = reader.GetString(3)
+            };
+        }
+
+        return null;
+    }
+
+    public User GetUserById(int id)
+    {
+        using var conn = Database.GetConnection();
+        conn.Open();
+
+        var cmd = conn.CreateCommand();
+        cmd.CommandText = "SELECT * FROM Users WHERE UserId = @id";
+        cmd.Parameters.AddWithValue("@id", id);
+
+        using var reader = cmd.ExecuteReader();
+
+        if (reader.Read())
+        {
+            return new User
+            {
+                UserId = reader.GetInt32(0),
+                Username = reader.GetString(1),
+                Password = reader.GetString(2),
+                Role = reader.GetString(3)
+            };
+        }
+
+        return null;
+    }
 }
