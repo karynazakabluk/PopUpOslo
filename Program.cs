443a5f557fa6feaf813e3299f580d1f8a6ec71de
MariaVilma12 <vilmashekar26@gmail.com>
2026-04-13 12:19:08 +0200

    added seed initialization

--- Program.cs
+++ Program.cs
@@ -7,8 +7,9 @@ class Program
     static void Main(string[] args)
     {
         Batteries.Init();
+        bool seed = args.Contains("--seed");
 
-        DatabaseInitializer.Initialize();
+        DatabaseInitializer.Initialize(seed);
 
         Console.WriteLine("Application started...");
         
